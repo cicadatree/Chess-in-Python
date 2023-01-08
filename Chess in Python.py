84ecb70da259412939fb927b9cf75bdd2742b2d2
cicadatree <me@brendanknowles.com>
2023-01-07 19:21:04 -0500

    added __str__() methods to classes. assigned piece objects directly to board positions (instead of assigning them to a variable, and then assigning the variable to the given board position)

--- Chess in Python.py
+++ Chess in Python.py
@@ -1,80 +1,123 @@
 from dataclasses import dataclass
-from typing import List
 from enum import Enum, auto
 
+class Colour(Enum):
+    WHITE = auto()
+    BLACK = auto()
+    def __str__(self) -> str:
+        if self.value == Colour.WHITE.value:
+            return 'W'
+        else:
+            return 'B'
+    __repr__ = __str__
+
+
+
 @dataclass
+class Player:
+    colour: Colour
+
 class ChessBoard:
-    board = [[None for j in range(8)] for i in range(8)]
+    board = [['' for j in range(8)] for i in range(8)]
     # define the initial board as a 2D array
     # board is a list of lists representing the state of the chessboard
     # None represents an empty square
     # the outer list represents the rows of the board (8 rows)
     # the inner list represents the columns of the board (8 columns)
+    def __str__(self) -> str:
+        ret = ""
+        for i in range(8):
+            ret = ret + str(self.board[i]) + "\n"
+        return ret
 
-class Colour(Enum):
-    WHITE = auto()
-    BLACK = auto()
+    def getPiece(self,userInput):
+        if len(userInput) > 2:
+            return "Invalid input - must be exactly 2 characters"
 
-@dataclass
-class PawnPiece: 
-    colour: Colour
-@dataclass
-class RookPiece:
-    colour: Colour
-@dataclass
-class KnightPiece: 
-    colour: Colour
-@dataclass
-class BishopPiece: 
-    colour: Colour
-@dataclass
-class KingPiece: 
-    colour: Colour
-@dataclass
-class QueenPiece: 
-    colour: Colour
+        for i in userInput:
+            for 
 
+class Piece:
+    colour: Colour
+    fullName: str
+    shortName: str
+    def __str__(self):
+        return str(self.colour) + self.shortName
+    __repr__ = __str__
+
+class PawnPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "Pawn"
+        self.shortName = "P"
+
+class RookPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "Rook"
+        self.shortName = "R"
+
+class KnightPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "Knight"
+        self.shortName = "N"
+    
+class BishopPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "Bishop"
+        self.shortName = "B"
+
+class KingPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "King"
+        self.shortName = "K"
+
+class QueenPiece(Piece):
+    def __init__(self,colour):
+        self.colour = colour
+        self.fullName = "Queen"
+        self.shortName = "Q"
+
+# GameState stores all relevant attributes of the given game's state.
+# Can even consider adding an attribute that saves the game's current state as an FEN string (https://www.chess.com/terms/fen-chess)
+
+class GameState:
+    gameBoard: ChessBoard
+    turnCounter: int = 0
+    whichTurn: Colour = Colour.WHITE
+
+game = GameState()
+game.gameBoard = ChessBoard()
+
+game.gameBoard.board[0][0] = RookPiece(Colour.WHITE)
+game.gameBoard.board[0][1] = KnightPiece(Colour.WHITE)
+game.gameBoard.board[0][2] = BishopPiece(Colour.WHITE)
+game.gameBoard.board[0][3] = QueenPiece(Colour.WHITE)
+game.gameBoard.board[0][4] = KingPiece(Colour.WHITE)
+game.gameBoard.board[0][5] = BishopPiece(Colour.WHITE)
+game.gameBoard.board[0][6] = KnightPiece(Colour.WHITE)
+game.gameBoard.board[0][7] = RookPiece(Colour.WHITE)
+
+for i in range(8):
+    game.gameBoard.board[1][i] = PawnPiece(Colour.WHITE)
+    game.gameBoard.board[6][i] = PawnPiece(Colour.BLACK)
+
+game.gameBoard.board[7][0] = RookPiece(Colour.BLACK)
+game.gameBoard.board[7][1] = KnightPiece(Colour.BLACK)
+game.gameBoard.board[7][2] = BishopPiece(Colour.BLACK)
+game.gameBoard.board[7][3] = QueenPiece(Colour.BLACK)
+game.gameBoard.board[7][4] = KingPiece(Colour.BLACK)
+game.gameBoard.board[7][5] = BishopPiece(Colour.BLACK)
+game.gameBoard.board[7][6] = KnightPiece(Colour.BLACK)
+game.gameBoard.board[7][7] = RookPiece(Colour.BLACK)
+    
 while True:
-    gameBoard = ChessBoard()
+    print("This game is chess. You are playing this game of chess. It is " + str(game.whichTurn) + "'s turn" + "\n")
+    print(game.gameBoard)
+    pieceSelect = input("What piece do you want to move? ")
+    if pieceSelect == 
 
-    whiteRook1 = RookPiece(Colour.WHITE)
-    gameBoard.board[0][0] = whiteRook1
-    whiteKnight1 = KnightPiece(Colour.WHITE)
-    gameBoard.board[0][1] = whiteKnight1
-    whiteBishop1 =  BishopPiece(Colour.WHITE)
-    gameBoard.board[0][2] = whiteBishop1
-    whiteQueen = QueenPiece(Colour.WHITE)
-    gameBoard.board[0][3] = whiteQueen
-    whiteKing = KingPiece(Colour.WHITE)
-    gameBoard.board[0][4] = whiteKing
-    whiteBishop2 = BishopPiece(Colour.WHITE)
-    gameBoard.board[0][5] = whiteBishop2
-    whiteKnight2 = KnightPiece(Colour.WHITE)
-    gameBoard.board[0][6] = whiteKnight2
-    whiteRook2 = RookPiece(Colour.WHITE)
-    gameBoard.board[0][7] = whiteRook2
-
-    # I need to figure out how to get pawn's assigned to discrete / distinct variables
-    for i in range(8):
-        gameBoard.board[1][i] = PawnPiece(Colour.WHITE)
-        gameBoard.board[6][i] = PawnPiece(Colour.BLACK)
-
-    blackRook1 = RookPiece(Colour.BLACK)
-    gameBoard.board[7][0] = blackRook1
-    blackKnight1 = KnightPiece(Colour.BLACK)
-    gameBoard.board[7][1] = blackKnight1
-    blackBishop1 = BishopPiece(Colour.BLACK)
-    gameBoard.board[7][2] = blackBishop1
-    blackQueen = QueenPiece(Colour.BLACK)
-    gameBoard.board[7][3] = blackQueen
-    blackKing = KingPiece(Colour.BLACK)
-    gameBoard.board[7][4] = blackKing
-    blackBishop2 = BishopPiece(Colour.BLACK)
-    gameBoard.board[7][5] = blackBishop2
-    blackKnight2 = KnightPiece(Colour.BLACK)
-    gameBoard.board[7][6] = blackKnight2
-    blackRook2 = RookPiece(Colour.BLACK)
-    gameBoard.board[7][7] = blackRook2
-
-    print(gameBoard.board)
     break
