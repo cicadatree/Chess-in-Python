f6a19f888ca9acea641123765addb95ccf2957b5
cicadatree <me@brendanknowles.com>
2023-01-12 20:51:36 -0500

    removed Player class (not needed). added global validation fuction (validateInput()) which validates that the user input is an int between 1 and 8 (for validating user inputs when choosing a piece to move)

--- Chess in Python.py
+++ Chess in Python.py
@@ -3,9 +3,27 @@
 # large-scale tasks to be completed (as of Jan 8, 2023):
 ## 1. Figure out the actual game engine
 ## 2. Figure out how to draw the game in a window (GUI)
-
+from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum, auto
+
+# global utlity function to check if the user input is 
+def validateInput(message) -> int:
+    # get input from user with message
+    userInput = input(message)
+    # check if userInput is a string
+    if isinstance(userInput, str):
+        try:
+            int(userInput)
+        except ValueError:
+            userInput = -1
+            pass
+        userInput = int(userInput)
+    # validate that the userInput is between 1 and 8
+    if userInput < 8 and userInput > 0:
+        return(userInput)
+
+    validateInput("TRY AGAIN IDIOT ")
 
 class Colour(Enum):
     # enumerate white and black
@@ -22,10 +40,6 @@ class Colour(Enum):
     # you have to explicitly tell python to reassign the new __str__ output as the the __repr__ output for the class
     # if you do not, it will continue to print the memory address of the given objecft
     __repr__ = __str__
-
-@dataclass
-class Player:
-    colour: Colour
 
 class ChessBoard:
     # define the initial board as a 2D array, where '' represents an empty square
@@ -59,7 +73,8 @@ class ChessBoard:
         self.board[7][6] = KnightPiece(Colour.BLACK)
         self.board[7][7] = RookPiece(Colour.BLACK)
 
-
+    def getPiece(self,x,y) -> Piece:
+        return print(self.board[x][y])
 
     # redefine the __str__ special function to print the chess board out with new lines after every outer list element
     def __str__(self):
@@ -116,7 +131,6 @@ class QueenPiece(Piece):
         self.shortName = "Q"
 
 # GameState stores all relevant attributes of the given game's state.
-# Can even consider adding an attribute that saves the game's current state as an FEN string (https://www.chess.com/terms/fen-chess)
 class GameState:
     gameBoard: ChessBoard = ChessBoard()
     turnCounter: int = 0
@@ -124,23 +138,6 @@ class GameState:
     # gameBoard is a ChessBoard-like object
     # turnCounter starts on 0 and should increment by 1 at the end of each turn.
     # whichColour indicates whose turn it is (starting with White by default)
-    def moves(self, gameBoard):
-        for i in range(len(gameBoard)):
-            for j in range(len(gameBoard[i])):
-                if j == PawnPiece:
-                    moves = []
-
-#############################
-### Jimbo says: try moving this logic for creating the board inside the constructor of the ChessBoard class
-### or even better make a function on the class that does the board creation and call that fuction in the constructor of the ChessBoard or the GameState
-### this way the function name can tell you (or another dev) what the block of code inside the function does. this makes for cleaner code with less comments!
-###
-### when writting clean code it can help to think about someone else coming and trying to use this code after you
-### some other dev wont expect to have to set up the pieces themselfs (like you are doing here) they would expect
-### the ChessBoard itself to set itself up by default or to at least have a setup function they could call to
-### Easily put all the pieces in there defalt locations. think about how libaries you consume are organized. and//
-#############################
-
 
 # assign game to a new instance of the GameState object, and then assign the ChessBoard() object as it's gameBoard
 game = GameState()
@@ -150,5 +147,10 @@ while True:
     print("\n")
     print("This game is called chess. There are two players; White (aka W) and Black (aka B). Each player starts with the same number and type of pieces.\n")
     print("It is " + str(game.whichTurn) + "'s turn \n" + "Here is the game board: \n")
-    print(game.gameBoard)
+
+    userInputX = validateInput("What is the X value for your piece? ")
+    userInputY = validateInput("What is the Y value for your piece? ")
+
+    game.gameBoard.getPiece(userInputX,userInputY)
+
     break
